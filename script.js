eed1023716c7af90289eb1ac4c8a3787e87a6fd2
sophie321123 <samanta.pcrun@gmail.com>
2025-05-10 01:49:40 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// تبدیل اعداد به فارسی
+document.body.innerHTML = document.body.innerHTML
+    .replace(/0/g, '۰')
+    .replace(/1/g, '۱')
+    .replace(/2/g, '۲')
+    .replace(/3/g, '۳')
+    .replace(/4/g, '۴')
+    .replace(/5/g, '۵')
+    .replace(/6/g, '۶')
+    .replace(/7/g, '۷')
+    .replace(/8/g, '۸')
+    .replace(/9/g, '۹');
